122bad8dcad13423a762925e90ed0503a1df1fda
zakariya1057 <zakaria2011@live.no>
2020-08-07 23:14:32 +0100

    Asda Categories. Parent/Child/Grand Parent

--- Stores/Asda/AsdaCategories.php
+++ Stores/Asda/AsdaCategories.php
@@ -2,19 +2,24 @@
 
 namespace Stores\Asda;
 
+use Exception;
 use Shared\Database;
 use Shared\Request;
 use Models\CategoryModel;
+use Models\ChildCategoryModel;
+use Models\GrandParentCategoryModel;
+use Models\ParentCategoryModel;
 
 class AsdaCategories {
 
-    private $logger,$request,$config,$database;
+    private $logger,$request,$config,$database,$site_id;
 
     function __construct($config,$logger,$database){
-        $this->request = new AsdaRequests($config,$logger);
+        $this->request  = new AsdaRequests($config,$logger);
         $this->database = $database;
-        $this->logger  = $logger;
-        $this->config  = $config;
+        $this->logger   = $logger;
+        $this->config   = $config;
+        $this->site_id  = $config->get('asda')->site_id;
     }
 
     public function categories($categories){
@@ -30,65 +35,78 @@ class AsdaCategories {
     }
 
     public function create_category($category_item){
-        $category_details = $this->category_information($category_item);
+        //Insert or select category item.
+        $category_details = $this->select_category($category_item,"grand_parent");
         $this->logger->debug("- Category: $category_details->name");
 
         foreach($category_item->subcategories as $department){
-            $this->create_department($department);
+            $this->create_department($department,$category_details->id);
         }
 
     }
 
-    public function create_department($department_item){
-        $department_details = $this->category_information($department_item);
+    public function create_department($department_item,$parent_id){
+        $department_item->parent_id = $parent_id;
+
+        $department_details = $this->select_category($department_item,"parent");
         $this->logger->debug("-- Department: $department_details->name");
 
         foreach($department_item->subcategories as $aisle){
-            $this->create_aisle($aisle);
+            $this->create_aisle($aisle,$department_details->id);
         }
 
     }
 
-    public function create_aisle($aisle){
-        $aisle_details = $this->category_information($aisle);
+    public function create_aisle($aisle,$parent_id){
+        $aisle->parent_id = $parent_id;
+
+        $aisle_details = $this->select_category($aisle,"child");
         $this->logger->debug("--- Aisle: $aisle_details->name");
 
         $shelf = new AsdaShelves($this->config,$this->logger,$this->database);
         $shelf->details($aisle_details);
 
-        // foreach($aisle->subcategories as $shelf){
-        //     $this->create_shelf($shelf);
-        // }
-
     }
 
-    // public function create_shelf($shelf){
-    //     $shelf_details = $this->category_information($shelf);
-    //     $this->logger->debug("\t\t\tShelf: $shelf_details->name");
-        
-    //     $asda_shelf = new AsdaShelf($this->config,$this->logger);
-    //     $asda_shelf->products($shelf);
-    // }
-
-    public function category_information($category){
+    public function select_category($category,$type){
         $category_store_id = $category->dimvalid;
         $category_name = $category->displayName;
+        $parent_id = $category->parent_id ?? null;
 
-        $category = new CategoryModel($this->database);
+        $insert_fields = [
+            'name' => $category_name,
+            'site_category_id' => $category_store_id,
+            'parent_id' => $parent_id,
+            'site_id' => $this->site_id 
+        ];
 
-        // $category_item = $category->where(["category_store_id" => $category_store_id]);
-        $category_item = null;
+        if($type == "grand_parent"){
+            $category = new GrandParentCategoryModel($this->database);
+            unset($insert_fields['parent_id']);
+        } elseif($type == "parent"){
+            $category = new ParentCategoryModel($this->database);
+        } elseif($type == "child"){
+            $category = new ChildCategoryModel($this->database);
+        } else {
+            throw new Exception("Unknown Category Type Found: $type");
+        }
 
-        if(is_null($category_item)){
-            $category_insert_id = $category->create(['store_category_id' => $category_store_id,'name' => $category_name]);
+        $category_item = $category->where(["site_category_id" => $category_store_id])->get();
+
+        if($category_item){
+            return $category_item;
+        } else {
+            $category_insert_id = $category->create($insert_fields);
 
             $category_item = new CategoryModel();
-            $category_item->store_category_id = $category_store_id;
-            $category_item->name = $category_name;
             $category_item->id = $category_insert_id;
-        } 
+            $category_item->name = $category_name;
+            $category_item->site_category_id = $category_store_id;
+            $category_item->parent_id = $parent_id;
 
-        return $category_item;
+            return $category_item;
+
+        }
 
     }
 
