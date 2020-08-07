cfff98a88e53d7ed200381ae7f04668889acf90d
zakariya1057 <zakaria2011@live.no>
2020-08-07 23:12:51 +0100

    Removing Unused Code From Category Model

--- Models/CategoryModel.php
+++ Models/CategoryModel.php
@@ -8,37 +8,19 @@ use Shared\Database;
 use Shared\Loggers;
 use Models\Model;
 
+//Store Name, Logo, Description, etc.
+
+// Cat Id, Name, Level(0,1,2),parent_id
+// Cat Id, Name, Level(1,2,3),parent_id
+
+//Select * from parent_categories inner join child_categories on child_categories.parent_id = parent_categories.id inner join products on products.parent_id = child_categories.id where store_type=1
+//SELECT * FROM categories C1 inner join categories C2 on C1.parent_id = C2.id inner join categories C3 on C2.parent_id = C3.id
+
+//Grandparent category, parent category, child category
+
 class CategoryModel extends Model {
-
-    public $database, $logger,$product;
-    
-    public $id,$name,$store_category_id;
-
-    function __construct($database=null){
-
-        if($database){
-            $this->database = $database;
-        }
-
-        $log = new Loggers();
-        $this->logger = $log->logger_handler;
-
-        $this->table("products");
-
-        $fields = [
-            "name" => [
-                "nullable" => false
-            ],
-            "store_category_id" => [
-                "nullable" => false,
-                "regex"    => "/^\w+$/"
-            ]
-        ];
-
-        $this->fields($fields);
-
-    }
-
+    public $database,$logger,$product;
+    public $id,$name,$site_category_id;
 }
 
 ?>
