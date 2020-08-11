0b5b8d244f0333b4a1517ad106156f8dd09a9dc6
Zakariya Mohummed <zakaria2011@live.no>
2020-08-11 20:33:31 +0100

    Main file, extended by sub files

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
+// DONE - Reviews Table
+// DONE - Related Products
+// TODO - Main, Locations Stores Table
+// TODO - Ingredients Table
+// TODO - Promotions Table
+
 use Shared\Config;
 use Shared\Loggers;
 use Shared\Database;
-use Stores\Asda\AsdaGroceries;
+
+use Stores\Asda\Asda;
 
 $config = new Config();
 $log = new Loggers();
@@ -19,18 +26,26 @@ $asda_conf = $config->get('asda');
 
 if($asda_conf->run){
     $logger->notice("Asda Scraping Start");
-    $asda = new AsdaGroceries($config,$logger,$database);
+    $asda = new Asda($config,$logger,$database);
 
     // Scrape all categories, including promotions. Ignore duplicates products.
     // This continues where it left off, remembring where it was last time it ran. It keeps its own run history. 
     // Last category it was on and last product.
+
     if($asda_conf->groceries){
+        //Get all product sold on site
         $asda->groceries();
     }
 
     //Searches For new promotions.
     if($asda_conf->promotions){
-        // $logger->notice("Asda Promotions Start");
+        //Get new promotions. Update old ones
+        $asda->promotions();
+    }
+
+    if($asda_conf->recommended){
+        //For each product in database. Get all similar Products.
+        $asda->recommended();
     }
 
     $logger->notice("Asda Scraping Complete");
