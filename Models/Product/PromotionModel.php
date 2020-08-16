4885e42f8da9f443adf0a77796108862b8d0e6fe
Zakariya Mohummed <zakaria2011@live.no>
2020-08-16 20:55:29 +0100

    Created Promotion Model

---
+++ Models/Product/PromotionModel.php
@@ -0,0 +1,33 @@
+<?php
+// Create Model Class. Use For Inserting Into Databas. Searching And Deleting.
+namespace Models\Product;
+
+use Models\Model;
+
+class PromotionModel extends Model {
+
+    public $name,$site_promotion_id,$url,$expires,$starts_at,$ends_at;
+
+    // Promotion Id, Name, URL, expires, starts_at,ends_at
+    function __construct($database=null){
+
+        parent::__construct($database);
+
+        $this->table("promotions");
+
+        $fields = [
+            'name' => [],
+            'site_promotion_id' => [],
+            'expires' => ['nullable' => true],
+            'starts_at' => ['nullable' => true],
+            'ends_at' => ['nullable' => true],
+            'url' => []
+        ];
+
+        $this->fields($fields);
+
+    }
+
+}
+
+?>
