7ddcbf68acbd604b20ebbe33ef66d42d1ecd5690
Zakariya Mohummed <zakaria2011@live.no>
2020-08-18 12:29:55 +0100

    Ignore Haram Code Added To Shared Asda File

--- Stores/Asda/Asda.php
+++ Stores/Asda/Asda.php
@@ -10,7 +10,7 @@ use Shared\WeightConverter;
 
 class Asda {
 
-    public $logger,$request,$config,$database,$endpoints,$env,$sanitize,$weight_converter,$site_type_id,$user_id,$city;
+    public $logger,$request,$config,$database,$endpoints,$env,$sanitize,$weight_converter,$site_type_id,$user_id,$city,$exclusions;
 
     function __construct($config,$logger,$database){
         $this->request = new Requests($config,$logger);
@@ -20,8 +20,8 @@ class Asda {
 
         $asda_conf = $this->config->get('asda');
 
-        $this->endpoints = $this->config->get("endpoints")->asda;
-        $this->env = $this->config->get("env");
+        $this->endpoints = $this->config->get('endpoints')->asda;
+        $this->env = $this->config->get('env');
 
         $this->sanitize = new Sanitize();
         $this->weight_converter = new WeightConverter();
@@ -29,6 +29,8 @@ class Asda {
         $this->site_type_id = $asda_conf->site_type_id;
         $this->user_id = $asda_conf->user_id;
         $this->city = $config->get('city');
+
+        $this->exclusions = $config->get('exclusions');
 
     }
 
@@ -50,6 +52,59 @@ class Asda {
     public function stores(){
         $stores = new AsdaStores($this->config,$this->logger,$this->database);
         $stores->stores();
+    }
+
+
+    //Shared Functionality
+    public function exclude_category($category_name){
+        $exclusions_list = join('|',$this->exclusions->categories->exclude );
+        preg_match("/$exclusions_list/i",$category_name,$category_matches);
+
+        if($category_matches){
+            //Haram Category Found
+            return true;
+        } else {
+            return false;
+        }  
+    }
+
+    public function exclude_product($product_name){
+        $exclusions_list = join('|', array_merge($this->exclusions->products->exclude,$this->exclusions->categories->exclude) );
+        preg_match("/$exclusions_list/i",$product_name,$product_matches);
+
+        if($product_matches){
+            //Haram Product Found
+            return true;
+        } else {
+            return false;
+        }   
+    }
+
+    public function product_possible_haram($product_name){
+        $exclusions_list = join('|', $this->exclusions->products->check );
+        preg_match("/$exclusions_list/i",$product_name,$product_matches);
+            
+        if($product_matches){
+            //Product May Be Haram
+            return true;
+        } else {
+            return false;
+        }   
+    }
+
+    public function haram_ingredients($ingredients){
+
+        foreach($ingredients as $ingredient_name){
+            $exclusions_list = join('|',$this->exclusions->ingredients->exclude);
+            preg_match("/$exclusions_list/i",$ingredient_name,$haram_matches);
+
+            if($haram_matches){
+                return true;
+            }
+        }
+
+        return false;
+
     }
 
 }
