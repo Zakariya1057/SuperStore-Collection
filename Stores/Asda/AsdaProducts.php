af3ba0d2e8b81f4c1a527ae7a84c13b8680f9c08
Zakariya Mohummed <zakaria2011@live.no>
2020-08-18 12:28:52 +0100

    Excluding Haram Products

--- Stores/Asda/AsdaProducts.php
+++ Stores/Asda/AsdaProducts.php
@@ -49,16 +49,21 @@ class AsdaProducts extends Asda {
                 
             }
 
-            //Product
-            $product_details->parent_category_id = $parent_category_id;
-            $product_details->database = $this->database;
-            $product_id = $product_details->save();
+            if($product_details){
+                //Product Added
+                $product_details->parent_category_id = $parent_category_id;
+                $product_details->database = $this->database;
+                $product_id = $product_details->save();
 
-            $this->reviews($product_id,$product_site_id);
-            $this->ingredients($product_id,$this->product_details);
+                $this->reviews($product_id,$product_site_id);
+                $this->ingredients($product_id,$this->product_details);
 
+                $this->logger->debug('Complete Product Added');
 
-            return $product_id;
+                return $product_id;
+            } else {
+                $this->logger->debug('Product Not Added');
+            }
 
         } else { 
             $this->logger->info("Product Found In Database: $product_site_id");
@@ -89,13 +94,56 @@ class AsdaProducts extends Asda {
         //Design and build product table, ingredients table,
 
         $item = $product_details->item;
+        $name = $item->name;
         $item_enrichment = $product_details->item_enrichment->enrichment_info;
         $rating_review = $item->rating_review;
 
         $this->logger->notice('--- Start Product('.$item->sku_id.'): '.$item->name .' ---');
 
         $product = new ProductModel();
-        $product->name = $item->name;
+        $product->name = $name;
+        $product->dietary_info = $item_enrichment->dietary_info ?? NULL;
+
+        if(!$this->exclude_product($name)){
+            $this->logger->debug('Stage 1. Product Not Exluded: '. $name);
+        } else {
+            $this->logger->debug('Stage 1. Product Exluded: '. $name);
+            return null;
+        }
+
+        //Check product name, if matches possible haram then double check
+        if(!$this->product_possible_haram($name)){
+            $this->logger->debug('Stage 2. Product Halal '. $name);
+        } else {
+            
+            $this->logger->debug('Stage 2. Product Maybe Haram. Halal/Vegan/Vegetarian Check');
+
+            if(!is_null($product->dietary_info)){
+                preg_match("/halal|vegetarian|vegan/i",$product->dietary_info,$halal_matches);
+
+                if($halal_matches){
+                    $this->logger->debug('Stage 2A. Product Halal');
+                } else {
+                    $this->logger->debug('Stage 2. Product Haram: '. $name);
+                    return;
+                }
+            } else {
+                $this->logger->debug('Stage 2. Product Haram: '. $name);
+                return;
+            }
+           
+        }
+
+        //Check product ingredients, if pork found then exlucde.
+        $ingredients = $this->ingredients_list($product_details);
+        if($this->haram_ingredients($ingredients)){
+            $this->logger->debug('Stage 3. Haram Ingredients Found: '. $name);
+            return;
+        } else {
+            $this->logger->debug('Stage 3. No Haram Ingredients Found: '. $name);
+        }
+
+
         $product->site_type_id = $this->site_type_id;
         $product->description = $item->description == '.' ? NULL : $item->description;
         $product->site_product_id = $item->sku_id;
@@ -111,7 +159,6 @@ class AsdaProducts extends Asda {
         $product->small_image = $this->product_image($image_id,150);
 
         $product->brand = $item->brand;
-        $product->dietary_info = $item_enrichment->dietary_info ?? NULL;
         $product->allergen_info = $item_enrichment->allergy_info_formatted_web ?? NULL;
 
         $product->storage = $item_enrichment->storage ?? NULL;
@@ -151,36 +198,36 @@ class AsdaProducts extends Asda {
 
         $this->logger->debug("Reviews Products ID: $product_site_id");
 
-        // if($this->env == "dev"){
+        if($this->env == "dev"){
             $reviews_response = file_get_contents(__DIR__."/../../Data/Asda/Reviews.json");
             $reviews_results = $this->request->parse_json($reviews_response);
             $this->process_reviews($product_id, $reviews_results->Results);
-        // } else {
-        //     $reviews_response = $this->request->request($reviews_endpoint);
-        //     $reviews_results = $this->request->parse_json($reviews_response);
+        } else {
+            $reviews_response = $this->request->request($reviews_endpoint);
+            $reviews_results = $this->request->parse_json($reviews_response);
 
-        //     $total_reviews = $reviews_results->TotalResults;
-        //     $this->logger->notice($total_reviews . ' Reviews Found');
+            $total_reviews = $reviews_results->TotalResults;
+            $this->logger->notice($total_reviews . ' Reviews Found');
 
-        //     if($total_reviews > 100){
-        //         $total_pages = ceil($total_reviews / 100 );
-        //     } else {
-        //         $total_pages = 1;
-        //     }
+            if($total_reviews > 100){
+                $total_pages = ceil($total_reviews / 100 );
+            } else {
+                $total_pages = 1;
+            }
 
-        //     $this->logger->notice("Total Review Pages: $total_pages");
+            $this->logger->notice("Total Review Pages: $total_pages");
 
-        //     for($review_page = 0;$review_page < $total_pages;$review_page++){
+            for($review_page = 0;$review_page < $total_pages;$review_page++){
 
-        //         $this->logger->debug("Reviews Page $review_page");
+                $this->logger->debug("Reviews Page $review_page");
 
-        //         $reviews_response = $this->request->request($reviews_endpoint . '&Limit=100&Offset=' . $review_page * 100);
-        //         $reviews_results = $this->request->parse_json($reviews_response);
+                $reviews_response = $this->request->request($reviews_endpoint . '&Limit=100&Offset=' . $review_page * 100);
+                $reviews_results = $this->request->parse_json($reviews_response);
 
-        //         $this->process_reviews($product_id, $reviews_results->Results);
-        //     }
+                $this->process_reviews($product_id, $reviews_results->Results);
+            }
             
-        // }
+        }
 
     }
 
@@ -213,23 +260,36 @@ class AsdaProducts extends Asda {
 
     public function ingredients($product_id, $product_data){
         //Store Product Ingredients
+
+        $ingredients_list = $this->ingredients_list($product_data);
+
+        print_r($ingredients_list);
+
+        foreach($ingredients_list as $ingredient_name){
+            $ingredient = new IngredientModel($this->database);
+            $ingredient->name = $ingredient_name;
+            $ingredient->product_id = $product_id;
+
+            $ingredient->save();
+        }
+
+    }
+
+    public function ingredients_list($product_data){
         $ingredients_response = $product_data->item_enrichment->enrichment_info->ingredients_formatted;
         $ingredients_list = explode(' , ',$ingredients_response);
 
-        print_r($ingredients_list);
+        $list = array();
 
         foreach($ingredients_list as $ingredient_item){
             $ingredient_name = preg_replace('/\s*\.?\s*$/','',$ingredient_item);
             if($ingredient_name != ''){
-                $ingredient = new IngredientModel($this->database);
-                $ingredient->name = $ingredient_name;
-                $ingredient->product_id = $product_id;
-    
-                $ingredient->save();
+                $list[] = $ingredient_name;
             }
 
         }
 
+        return $list;
     }
 
 }
