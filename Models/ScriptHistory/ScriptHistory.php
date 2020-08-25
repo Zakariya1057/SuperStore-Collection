f2be51c94ecc32c943303c8bc99cdc51bcae630e
Zakariya Mohummed <zakaria2011@live.no>
2020-08-25 11:20:52 +0100

    ScriptHistory Model, Keeping Track

---
+++ Models/ScriptHistory/ScriptHistory.php
@@ -0,0 +1,63 @@
+<?php
+// Create Model Class. Use For Inserting Into Databas. Searching And Deleting.
+namespace Models\ScriptHistory;
+
+use Models\Model;
+
+//id,site_name, grand_parent_category_index, parent_category_index, child_category_index, product_index, error_file, error_message, host, created_at, updated_at
+
+class ScriptHistory extends Model {
+
+    public $database, $logger,$product;
+    
+    public $site_type_id, $grand_parent_category_index, $parent_category_index, $child_category_index, $product_index, $error_file, $error_message,$error_line_number;
+    function __construct($database=null){
+
+        parent::__construct($database);
+
+        $this->table("script_histories");
+
+        $fields = [
+
+            'site_type_id' => [ 
+                'type' => 'int'
+            ],
+            
+            'grand_parent_category_index' => [
+                'nullable'=> true,
+                'type' => 'int'
+            ],
+            'parent_category_index' => [
+                'nullable'=> true,
+                'type' => 'int'
+            ],
+            'child_category_index' => [
+                'nullable'=> true,
+                'type' => 'int'
+            ],
+
+            'product_index' => [
+                'nullable'=> true,
+                'type' => 'int'
+            ],
+
+            'error_file' => [
+                'nullable'=> true,
+            ],
+            'error_message' => [
+                'nullable'=> true,
+            ],
+            'error_line_number' => [
+                'nullable'=> true,
+                'type' => 'int'
+            ],
+            
+        ];
+
+        $this->fields($fields);
+
+    }
+
+}
+
+?>
