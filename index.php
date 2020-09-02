efd9bf2df2cdb9f0bf2f8da5e3e2ad6af79a8c0f
Zakariya Mohummed <zakaria2011@live.no>
2020-09-02 08:29:52 +0100

    New Item In Index TODO List

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     // DONE - Exclude Haram Products
     // DONE - Resume Scraping
     // DONE - Store Last Run Details In Database. Saving To File Causing Errors: Failed to open stream: Too many open files
+    // TODO - Download Product Image. Locally/S3 Bucket
     // TODO - Use Parent Category To See If Haram: 1000017525663 - If meat in parent category
     // TODO - New Promotions
 
