0b1748bfffbb9e42867c5a7bfc8655cc0ebb24da
Zakariya Mohummed <zakaria2011@live.no>
2020-09-05 22:07:20 +0100

    Image Saving Script

---
+++ Shared/Image.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Shared;
+
+use Exception;
+use Aws\S3\S3Client; 
+use Aws\Credentials\Credentials;
+
+class Image {
+
+    private $logger, $img_config, $aws_credentials, $request;
+
+    function __construct($config,$logger,Requests $request){
+        $this->logger = $logger;
+
+        $this->img_config = $config->get('images');
+        $this->host = $this->img_config->host;
+
+        $credentials = $this->img_config->aws->credentials;
+        $this->aws_credentials = new \Aws\Credentials\Credentials($credentials->key, $credentials->secret);
+
+        $this->request = $request;
+    }
+
+    public function save($name, $image_url,$size='',$type='products'){
+
+        $name = "{$name}_{$size}.jpg";
+        $type = $this->type_directories($type);
+        $image = $this->get_image($image_url);
+
+        $this->logger->debug("Image URL: $image_url");
+
+        if(is_null($image)){
+            return;
+        }
+
+        if($this->img_config->saving_location == 'local'){
+            $this->logger->debug('Saving Images On Local Machine');
+            $file_location = $this->store_local($name, $image,$type);
+        } else {
+            $this->logger->debug('Saving Images On AWS S3 Bucket');
+            $file_location = $this->store_aws($name, $image,$type);
+        }
+
+        $this->logger->debug("Saving $type Images To $file_location");
+        
+        return $file_location;
+    }
+
+    private function store_local($name, $image,$type){
+        //Store On Local Server
+
+        $dir = $this->img_config->local->location;
+        
+        if(!file_exists($dir)){
+            $this->logger->debug('Creating Image Directory: ' . $dir);
+            mkdir($dir, 0777, true);
+        }
+
+        $relative_file_location = "$type/$name";
+
+        $file_location = "$dir/$relative_file_location";
+
+        // $image = $this->get_image($image_url);
+
+        if(!file_put_contents($file_location, $image)){
+            throw new Exception("Failed To Save Image: $file_location");
+        }
+
+        // $file_name = $this->host . "/images/$name";
+        // $this->logger->debug("Saved Image: $image_url -> $file_location");
+
+        return $relative_file_location;
+    }
+
+    private function store_aws($name, $image,$type){
+        // Store On S3 Bucket
+        $aws = $this->img_config->aws;
+
+        $file_location = "$type/$name";
+
+        //Create a S3Client
+        $s3Client = new S3Client([
+            'version' => $aws->version,
+            'region'  => $aws->region,
+            'credentials' => $this->aws_credentials
+        ]);
+
+        $s3Client->putObject([
+            'Bucket' => 'superstore.images',
+            'Key' => $file_location,
+            'Body' => $image,
+        ]);
+
+        return $file_location;
+    }
+
+    private function type_directories($type){
+        $locations = $this->img_config->type_locations;
+        
+        strtolower($type);
+
+        if(property_exists($locations, $type)){
+            return $locations->{$type};
+        } else {
+            throw new Exception('Unknown Image Saving Option: '.$type);
+        }
+    }
+
+    private function get_image($url){
+        try {
+            return $this->request->request($url);
+        } catch (Exception $e){
+            $this->logger->error('Failed To Get Image');
+            return;
+        }
+    }
+
+}
+
+?>
