e4bf2fbb3452f0cfe67940e764b85c16b215aa7d
Zakariya Mohummed <zakaria2011@live.no>
2020-11-17 13:01:31 +0000

    Monitor store details: hours, facilities

--- Stores/Asda/AsdaMonitorStores.php
+++ Stores/Asda/AsdaMonitorStores.php
@@ -3,6 +3,7 @@
 namespace Stores\Asda;
 
 use Exception;
+use Models\Store\FacilitiesModel;
 use Models\Store\StoreModel;
 
 class AsdaMonitorStores extends Asda {
@@ -15,8 +16,6 @@ class AsdaMonitorStores extends Asda {
     }
 
     public function monitor_stores(){
-        // Check store details
-        // Store Hours, Facilities, etc
 
         $this->logger->debug('Store Monitoring');
 
@@ -33,7 +32,7 @@ class AsdaMonitorStores extends Asda {
 
             $this->logger->notice("------- Asda Store Start -----------");
             $this->logger->debug("Last Checked: {$store->time_difference} Hours Ago");
-            $this->logger->debug("Store: [{$store->id}] {$store->name}");
+            $this->logger->debug("Updating Store: [{$store->id}] {$store->name}");
             $this->check_store_change($store);
             $this->logger->notice("------- Asda Store Complete --------");
         }
@@ -45,24 +44,43 @@ class AsdaMonitorStores extends Asda {
         
         $new_store = $asda_store->page_store_details($store->url);
 
-        // Check opening hours
-        // Check facilities
-        // Store Image, description, image, google_url, image ulr
+        $this->database->start_transaction();
 
-        $product_changed = false;
+        // Update hours
+        foreach($new_store->opening_hours as $hour){
+            $hour
+            ->where(['store_id' => $store->id, 'day_of_week' => $hour->day_of_week])
+            ->update(['opens_at' => $hour->opens_at, 'closes_at' => $hour->closes_at, 'closed_today' => $hour->closed_today]);
+        }
 
-        $monitor_fields = ['name', 'description', 'store_image'];
-        // Decode fields
-        foreach($monitor_fields as $field){
-            if($store->{$field} != $new_store->{$field}){
-                $this->logger->notice("Store $field changed: {$store->{$field}} -> {$new_store->{$field}}");
-                $product_changed = true;
+        // Update facilities
+        $found_facilities = [];
+
+        foreach($new_store->facilities as $facility){
+            // Delete all facilities not found here outside.
+            $facility->insert_ignore = 1;
+            $facility->store_id = $store->id;
+            $facility->save();
+
+            $store_facility = $facility->where(['store_id' => $store->id, 'name' => $facility->name])->get()[0] ?? null;
+            if($store_facility){
+                $found_facilities[] = $store_facility->id;
             }
         }
 
-        if($product_changed){
-            $this->logger->notice('Store Details Changed');
+        if(count($found_facilities) > 0){
+            $facility = new FacilitiesModel($this->database);
+            $facility->where_not_in('id', $found_facilities)->delete();
         }
+
+        // Update details
+        $this->store->where(['id' => $store->id])->update([
+            'name' => $new_store->name,
+            'description' => $new_store->description,
+            'last_checked' => date('Y-m-d H:i:s')
+        ]);
+
+        $this->database->commit_transaction();
 
     }
 
