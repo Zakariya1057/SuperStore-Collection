22e81507e6f1d5ba48a5c1038a290b6695c84507
Zakariya Mohummed <zakaria2011@live.no>
2020-11-17 13:03:35 +0000

    Monitor promotions, new or deleted

--- Stores/Asda/AsdaMonitorProducts.php
+++ Stores/Asda/AsdaMonitorProducts.php
@@ -11,7 +11,7 @@ use Models\Shared\UserModel;
 
 class AsdaMonitorProducts extends Asda {
 
-    public $notification, $product, $review, $recommended;
+    public $notification, $product, $review, $recommended, $user_info, $product_promotions;
 
     function __construct($config,$logger,$database,$remember, $notification){
         parent::__construct($config,$logger,$database,$remember);
@@ -19,6 +19,8 @@ class AsdaMonitorProducts extends Asda {
         $this->product = new ProductModel($this->database);
         $this->review = new AsdaReviews($config, $logger, $database, null);
         $this->recommended = new AsdaRecommended($config, $logger, $database, null);
+        $this->user_info = new UserModel($this->database);
+        $this->product_promotions = new AsdaPromotions($config, $logger, $database, null);
     }
 
     public function monitor_products(){
@@ -28,8 +30,8 @@ class AsdaMonitorProducts extends Asda {
         ->join('grocery_list_items', 'grocery_list_items.product_id', 'products.id')
         ->join('monitored_products', 'monitored_products.product_id', 'products.id')
         ->join('favourite_products', 'favourite_products.product_id', 'products.id')
-        ->where_raw(['TIMESTAMPDIFF(HOUR, `last_checked`, NOW()) > 3'])
-        // ->where_raw(['products.id = 11'])
+        // ->where_raw(['TIMESTAMPDIFF(HOUR, `last_checked`, NOW()) > 3'])
+        ->where_raw(['products.id = 18'])
         ->group_by('products.id')
         ->order_by('num_monitoring')
         // ->limit(1)
@@ -44,7 +46,7 @@ class AsdaMonitorProducts extends Asda {
 
             $this->logger->notice("------- Asda Product Start -----------");
             $this->logger->debug("Last Checked: {$product_item->time_difference} Hours Ago");
-            $this->logger->debug("Product: [{$product_item->id}] {$product_item->name}");
+            $this->logger->debug("Updating Product: [{$product_item->id}] {$product_item->name}");
             $this->check_product_change($product_item);
             $this->logger->notice("------- Asda Product Complete --------");
         }
@@ -58,40 +60,87 @@ class AsdaMonitorProducts extends Asda {
         $new_product = $asda_product->product_details($product_item->site_product_id);
 
         $notify_user = true;
-        $product_changed = false;
+        $send_notification = false;
 
-        $monitor_fields = ['name', 'description', 'price','site_product_id'];
+        $notification_type = 'product';
+        $promotion_type = '';
+
+        $monitor_fields = [
+            'name',
+            'description',
+            'large_image',
+            'small_image',
+            'price',
+            'weight',
+            'brand',
+            'dietary_info',
+            'allergen_info',
+            'url',
+            'storage',
+            'site_product_id',
+        ];
+
         $update_fields  = ['last_checked' => date('Y-m-d H:i:s')];
         
         foreach($monitor_fields as $field){
             if($product_item->{$field} != $new_product->{$field}){
                 $this->logger->notice("Product $field changed: {$product_item->{$field}} -> {$new_product->{$field}}");
-                $product_changed = true;
+                $send_notification = true;
                 $notify_user = $field == 'price' ? true : false;
                 $update_fields[$field] = $new_product->{$field};
             }
         }
+        
+        $notify_user = true;
+        // $notification_type = 'promotion';
 
+        // New promotion
+        if(is_null($product_item->promotion_id) && !is_null($new_product->promotion)){
+            $this->logger->notice('New Product Promotion Found: '.$new_product->promotion_id);
+            $notification_type = 'promotion';
+            $send_notification = true;
+            $promotion_type = 'new';
+            $update_fields['promotion_id'] = $new_product->promotion_id;
+            $new_product->promotion->content = $this->product_promotions->promotion_calculator($new_product->promotion_id, $new_product->promotion->name);
+            // Notify User. New Promotion
+        } elseif( !is_null($product_item->promotion_id) && is_null($new_product->promotion) ){
+            $this->logger->notice('Product promotion removed');
+            $notification_type = 'promotion';
+            $send_notification = true;
+            $promotion_type = 'delete';
+            $update_fields['promotion_id'] = NULL;
+            // Notify User. Promotion Expires
+        } 
+        
         $notify_user = true;
         $this->database->start_transaction();
 
         // Check to see for new reviews
-        $this->review->create_review($product_item->id, $new_product->site_product_id);
+        // $this->review->create_review($product_item->id, $new_product->site_product_id);
         // Check to see if recommened changed
-        $this->recommended->product_recommended($product_item->id, $new_product->site_product_id);
-        
-        // Check to see if promotion still exists
+        // $this->recommended->product_recommended($product_item->id, $new_product->site_product_id);
 
         $this->product->where(['id' => $product_item->id])->update($update_fields);
 
-        if($product_changed){
+        if($send_notification){
             $this->logger->info('Updating Product Information For: ' . $product_item->id);
+
+            if($notification_type == 'product'){
+                // Notify new promotion, price change.
+                if($notify_user){
+                    $this->notify_product_changes($product_item, $notification_type);
+                }
+            } else {
+                if($promotion_type == 'delete'){
+                    $promotion = (object)['id' => (int)$product_item->promotion_id];
+                    $new_product->promotion = $promotion;
+                }
+
+                $this->notify_promotion_changes($new_product->promotion, $promotion_type == 'delete');
+            }
+
         } else {
             $this->logger->info('Product Not Changed For: ' . $product_item->id);
-        }
-
-        if($notify_user){
-            $this->notify_product_changes($product_item);
         }
 
         $this->database->commit_transaction();
@@ -103,7 +152,6 @@ class AsdaMonitorProducts extends Asda {
         $favourite = new FavouriteModel($this->database);
         $grocery   = new GroceryListItemModel($this->database);
         $monitor   = new MonitoredProductModel($this->database);
-        $user_info = new UserModel($this->database);
 
         $favourites_users = $favourite->where(['product_id' => $product_item->id])->group_by('user_id')->get();
         $grocery_users    = $grocery->join('grocery_lists', 'grocery_lists.id', 'grocery_list_items.list_id')->where(['product_id' => $product_item->id])->group_by('user_id')->get();
@@ -118,10 +166,10 @@ class AsdaMonitorProducts extends Asda {
             if(!key_exists($user_id, $unique_users)){
                 $unique_users[$user_id] = 1;
 
-                $user = $user_info->where(['id' => $user_id])->get()[0] ?? null;
+                $user = $this->user_info->where(['id' => $user_id])->get()[0] ?? null;
 
                 if($user && $user->send_notifications){
-                    $this->notification->notify_change($product_item, $user);
+                    $this->notification->notify_change($product_item, $user, 'product');
                 } else {
                     throw new Exception('User not found in database: ' . $user_id);
                 }
@@ -129,6 +177,18 @@ class AsdaMonitorProducts extends Asda {
             }
 
         }
+    }
+
+    public function notify_promotion_changes($promotion, $delete){
+        // Loop thorugh all users, notify new notification.
+        $users = $this->user_info->where_raw(['send_notifications is true', 'notification_token IS NOT NULL'])->get();
+        
+        if( (property_exists($promotion, 'new') && $promotion->new) || !property_exists($promotion, 'new')){
+            foreach($users as $user){
+                $this->notification->notify_change($promotion, $user, 'promotion', $delete);
+            }
+        }
+
     }
 
 }
