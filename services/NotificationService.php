ea3d2d724fdc122f642c490b3c97830f2e83c5c8
Zakariya Mohummed <zakaria2011@live.no>
2021-06-14 19:47:52 +0100

    Notification - Certificate -> Token (API). Fixed

--- services/NotificationService.php
+++ services/NotificationService.php
@@ -2,59 +2,82 @@
 
 namespace Services;
 
-use Exception;
+use Firebase\JWT\JWT;
 use Monolog\Logger;
 
 class NotificationService {
 
-    private $logger;
-    private $file_path, $pass_phrase, $url;
+    private $logger, $request;
+    
+    private $bundle_id, $key, $key_id, $url;
 
     function __construct(ConfigService $config_service, Logger $logger){
         $this->logger = $logger;
 
-        $this->file_path = __DIR__ .'/../'. $config_service->get('notification.file_path');
-        $this->pass_phrase = $config_service->get('notification.pass_phrase');
-        $this->url = $config_service->get('notification.url');
+        $notification_config = $config_service->get('notification');
+
+        $this->key = file_get_contents( __DIR__ .'/../'. $notification_config->file_path);
+        $this->url = $notification_config->url;
+
+        $this->bundle_id = $notification_config->bundle_id;
+        $this->key_id = $notification_config->key_id;
+
+        $this->request = new RequestService($config_service, $logger);
     }
 
     // Send notification to user phone.
     function send_notification($user, $data, $message){
-        $notification_token = $user->notification_token;
+        // $notification_token = $user->notification_token;
+        $device_token = 'bfebc29a163990bd3ab029e7d94ae37e85bc5264e05fbfaa43681f2ee8cfd03e';
 
         $this->logger->info("Sending Notification To: [{$user->id}] {$user->name}");
-        $this->logger->info("Notification Token: $notification_token");
-        
-        $ctx = stream_context_create();
-        stream_context_set_option($ctx, 'ssl', 'local_cert', $this->file_path);
-        stream_context_set_option($ctx, 'ssl', 'passphrase', $this->pass_phrase);
-        
-        $fp = stream_socket_client('ssl://'. $this->url , $err, $errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+        $this->logger->info("Device Token: $device_token");
 
-        // Create the payload body
-        $body['aps'] = array(
-            'badge' => +1,
-            'alert' => $message,
-            'sound' => 'default',
-            'data' => (array)$data
+        $payload = $this->generate_payload($message, $data);
+        
+        $headers = $this->generate_headers();
+        
+        $url = $this->url . $device_token;
+
+        $this->request->request($url, 'POST', $payload, $headers);
+
+        $this->logger->notice('Notification Sent Successfully');
+
+    }
+
+    private function generate_token(){
+        $payload = array(
+            "kid" => "Q9JS5DX954",
+            "iss" => "5FCM3X5N8A",
+            "alg" => "ES256",
+            "iat" => time()
         );
-        
-        $payload = json_encode($body);
-        
-        // Build the binary notification
-        $msg = chr(0) . pack('n', 32) . pack('H*', $notification_token) . pack('n', strlen($payload)) . $payload;
-        
-        // Send it to the server
-        $result = fwrite($fp, $msg, strlen($msg));
-        
-        if (!$result){
-            throw new Exception('Failed To Send User Notification');
-        } else {
-            $this->logger->notice('Notification Sent Successfully');
-        }
 
-        fclose($fp);
+        return (string) JWT::encode($payload, $this->key, 'ES256', $this->key_id);
+    }
 
+    private function generate_headers(){
+        $token = $this->generate_token();
+
+        $this->logger->debug('Notification Token: ' . $token);
+
+        return [
+            "Authorization" =>  "Bearer $token", 
+            "apns-topic" => $this->bundle_id, 
+            "apns-expiration" =>  "0",
+            "apns-priority" => "10",
+        ];
+    }
+
+    private function generate_payload($message, $data){
+        return [ 
+            'aps' => [
+                'badge' => +1,
+                'alert' => $message,
+                'sound' => 'default',
+                'data' => (array)$data
+            ]
+        ];
     }
 
 }
